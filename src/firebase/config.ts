b19a0f0f24d9d2261e20b7791b0cc6a4b4bc96b8
Theerapan C <theerapanMASCI@gmail.com>
2025-10-27 06:58:54 +0000

    กรุณาเชื่อมต่อข้อมูลกับ firestore database,Project name KPI insight Proj

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:217126069659:web:819c77310e5197801fad3e",
   "apiKey": "AIzaSyCX0r-VqgUjyGaPSXPyyKuWg3tKhE95V8A",
   "authDomain": "studio-2604565165-2f0b9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "217126069659"
+  "storageBucket": "studio-2604565165-2f0b9.appspot.com",
+  "messagingSenderId": "217126069659",
+  "measurementId": "G-5G65G3E74Y"
 };
